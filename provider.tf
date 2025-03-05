aa972b266947868c87052721358b8c66b3702be0
BhavyaAudisri <somisetty.bhavya@gmail.com>
2025-03-05 21:06:06 +0530

    docker

--- provider.tf
+++ provider.tf
@@ -10,6 +10,4 @@ terraform {
 provider "aws" {
   # Configuration options
   region = "us-east-1"
-  access_key = "AKIA4MTWLMWBND4UZCXH"
-  secret_key = "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"
 }
