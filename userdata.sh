806365a6420790fb698dc5bf8a817c60c7e5d9b5
BhavyaAudisri <somisetty.bhavya@gmail.com>
2025-03-07 14:51:14 +0530

    k8-resources

--- userdata.sh
+++ userdata.sh
@@ -2,15 +2,12 @@
 ARCH=amd64
 PLATFORM=$(uname -s)_$ARCH
 
-# Partitioning Volumes
-
 growpart /dev/nvme0n1 4
 lvextend -l +50%FREE /dev/RootVG/rootVol
 lvextend -l +50%FREE /dev/RootVG/varVol
 xfs_growfs /
 xfs_growfs /var
 
-# Docker installation
 dnf -y install dnf-plugins-core
 dnf config-manager --add-repo https://download.docker.com/linux/rhel/docker-ce.repo
 dnf install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y
@@ -19,13 +16,11 @@ systemctl enable docker
 usermod -aG docker ec2-user
 
 #kubectl installation
-
 curl -O https://s3.us-west-2.amazonaws.com/amazon-eks/1.32.0/2024-12-20/bin/linux/amd64/kubectl
 chmod +x ./kubectl
 mv kubectl /usr/local/bin/kubectl
 
-#eksctl installation
-
+#eksctl
 curl -sLO "https://github.com/eksctl-io/eksctl/releases/latest/download/eksctl_$PLATFORM.tar.gz"
 tar -xzf eksctl_$PLATFORM.tar.gz -C /tmp && rm eksctl_$PLATFORM.tar.gz
-sudo mv /tmp/eksctl /usr/local/bin
+mv /tmp/eksctl /usr/local/bin
