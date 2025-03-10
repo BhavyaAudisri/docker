a365399d1cac0d0d45439fa284b7421a0076ca03
BhavyaAudisri <somisetty.bhavya@gmail.com>
2025-03-10 19:34:23 +0530

    kube-resources

--- userdata.sh
+++ userdata.sh
@@ -24,3 +24,8 @@ mv kubectl /usr/local/bin/kubectl
 curl -sLO "https://github.com/eksctl-io/eksctl/releases/latest/download/eksctl_$PLATFORM.tar.gz"
 tar -xzf eksctl_$PLATFORM.tar.gz -C /tmp && rm eksctl_$PLATFORM.tar.gz
 mv /tmp/eksctl /usr/local/bin
+
+#kube-ns install
+sudo git clone https://github.com/ahmetb/kubectx /opt/kubectx
+sudo ln -s /opt/kubectx/kubectx /usr/local/bin/kubectx
+sudo ln -s /opt/kubectx/kubens /usr/local/bin/kubens
